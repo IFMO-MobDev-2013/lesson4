621bdb0bb07db6f76b818b890bdd82d94bed048e
Ruslan <akhundov2@gmail.com>
2013-10-09 00:27:10 +0400

    Akhundov Ruslan 2537

---
+++ src/MainActivity.java
@@ -0,0 +1,15 @@
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Руслан
+ * Date: 09.10.13
+ * Time: 0:22
+ * To change this template use File | Settings | File Templates.
+ */
+public class MainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}

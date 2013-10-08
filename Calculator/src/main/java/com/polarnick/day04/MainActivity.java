a04499a50ed981c8bd8f872af2edfca17389d881
Polarniy Nickolay <PolarHare@gmail.com>
2013-10-08 22:53:47 +0400

    GUI sketch added

---
+++ Calculator/src/main/java/com/polarnick/day04/MainActivity.java
@@ -0,0 +1,17 @@
+package com.polarnick.day04;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Date: 16.09.13
+ *
+ * @author Nickolay Polyarniy aka PolarNick
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}

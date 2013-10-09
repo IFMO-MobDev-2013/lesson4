36399adcf168ed897182373c176d53c677b2acfb
Ruslan <akhundov2@gmail.com>
2013-10-09 22:57:47 +0400

    Akhundov Ruslan 2537

---
+++ src/tests/Tests.java
@@ -0,0 +1,15 @@
+package tests;
+
+import com.example.lesson4.Parser;
+import
+
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Руслан
+ * Date: 09.10.13
+ * Time: 21:51
+ * To change this template use File | Settings | File Templates.
+ */
+public class Tests {
+}

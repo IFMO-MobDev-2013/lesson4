6d7d483172f2957e03a92fe78be2852e94fc5d0a
Ruslan <akhundov2@gmail.com>
2013-10-09 18:50:48 +0400

    version 1

--- src/com/example/lesson4/MainActivity.java
+++ src/com/example/lesson4/MainActivity.java
@@ -16,43 +16,48 @@ import com.example.lesson4.R;
  */
 public class MainActivity extends Activity {
     String input = "";
-    boolean button_lock = false;
+    boolean operation_lock = false;
+    boolean num_lock = false;
     TextView text;
-    int balance = 0;
+    int i, scope_balance = 0;
+    Parser parser;
 
-    private void operationButtonClicked(char operation){
-        if (!button_lock){
-            input += " " + operation + " ";
-            button_lock = true;
-            text.setText(input);
+    private void operationButtonClicked(char operation) {
+        if (!operation_lock) {
+            input += operation;
+            operation_lock = true;
+            text.append("" + operation);
+            num_lock = false;
         }
 
     }
 
-    private void numericButtonClicked(int num){
-        input += num;
-        button_lock = false;
-        text.setText(input);
+    private void numericButtonClicked(int num) {
+        if (!num_lock) {
+            input += num;
+            operation_lock = false;
+            text.append("" + num);
+        }
     }
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
 
-        Button[] numerics = new Button[10];
-        numerics[0] = (Button) findViewById(R.id.num0Button);
-        numerics[1] = (Button) findViewById(R.id.num1Button);
-        numerics[2] = (Button) findViewById(R.id.num2Button);
-        numerics[3] = (Button) findViewById(R.id.num3Button);
-        numerics[4] = (Button) findViewById(R.id.num4Button);
-        numerics[5] = (Button) findViewById(R.id.num5Button);
-        numerics[6] = (Button) findViewById(R.id.num6Button);
-        numerics[7] = (Button) findViewById(R.id.num7Button);
-        numerics[8] = (Button) findViewById(R.id.num8Button);
-        numerics[9] = (Button) findViewById(R.id.num9Button);
+        Button[] numeric = new Button[10];
+        numeric[0] = (Button) findViewById(R.id.num0Button);
+        numeric[1] = (Button) findViewById(R.id.num1Button);
+        numeric[2] = (Button) findViewById(R.id.num2Button);
+        numeric[3] = (Button) findViewById(R.id.num3Button);
+        numeric[4] = (Button) findViewById(R.id.num4Button);
+        numeric[5] = (Button) findViewById(R.id.num5Button);
+        numeric[6] = (Button) findViewById(R.id.num6Button);
+        numeric[7] = (Button) findViewById(R.id.num7Button);
+        numeric[8] = (Button) findViewById(R.id.num8Button);
+        numeric[9] = (Button) findViewById(R.id.num9Button);
 
-        Button plusButton = (Button)findViewById(R.id.plusButton);
-        Button subtracktButton = (Button)findViewById(R.id.subtractButton);
+        Button plusButton = (Button) findViewById(R.id.plusButton);
+        Button subtractButton = (Button) findViewById(R.id.subtractButton);
         Button timesButton = (Button) findViewById(R.id.timesButton);
         Button divisionButton = (Button) findViewById(R.id.divisionButton);
         Button moduleDivisionButton = (Button) findViewById(R.id.moduleDivisionButton);
@@ -62,6 +67,7 @@ public class MainActivity extends Activity {
         Button resetButton = (Button) findViewById(R.id.resetButton);
         Button deleteButton = (Button) findViewById(R.id.deleteButton);
         Button unaryOpButton = (Button) findViewById(R.id.unaryOpButton);
+        Button computeButton = (Button) findViewById(R.id.computeButton);
         text = (TextView) findViewById(R.id.textView);
 
         plusButton.setOnClickListener(new View.OnClickListener() {
@@ -71,7 +77,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        subtracktButton.setOnClickListener(new View.OnClickListener() {
+        subtractButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 operationButtonClicked('-');
@@ -96,6 +102,111 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 operationButtonClicked('%');
+            }
+        });
+
+        for (i = 0; i < 10; i++) {
+            numeric[i].setOnClickListener(new View.OnClickListener() {
+                final int x = i;
+
+                @Override
+                public void onClick(View v) {
+                    numericButtonClicked(x);
+                }
+            });
+        }
+
+        dotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        leftScopeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (operation_lock) {
+                    scope_balance++;
+                    input += "(";
+                    text.append("(");
+                }
+            }
+        });
+
+        rightScopeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (scope_balance > 0 &&
+                        (Character.isDigit(input.charAt(input.length() - 1)) || input.charAt(input.length()) == ')')) {
+                    scope_balance--;
+                    input += ")";
+                    num_lock = true;
+                    operation_lock = false;
+                    text.append(")");
+                }
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!input.isEmpty()) {
+                    input = input.substring(0, input.length() - 1);
+                    text.setText(input);
+                }
+            }
+        });
+
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input = "";
+                text.setText(input);
+            }
+        });
+
+        unaryOpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (input.length() > 0) {
+                    int i = input.length() - 1;
+                    if (input.charAt(i) == ')') {
+                        i--;
+                        while (i >= 0 && (input.charAt(i) >= '0' && input.charAt(i) <= '9' || input.charAt(i) == '.')) {
+                            i--;
+                        }
+                        if (i >= 1 && input.charAt(i) == '-' && input.charAt(i - 1) == '(') {
+                            input = input.substring(0, i - 1) + input.substring(i + 1, input.length() - 1);
+                        }
+                    } else {
+                        while (i >= 0 && (input.charAt(i) >= '0' && input.charAt(i) <= '9' || input.charAt(i) == '.')) {
+                            i--;
+                        }
+                        if (i != input.length() - 1) {
+                            i++;
+                            input = input.substring(0, i) + "(-" + input.substring(i, input.length()) + ")";
+                        }
+
+                    }
+
+                }
+                text.setText(input);
+            }
+        });
+        parser = new Parser();
+        computeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    for (int i = 0; i < scope_balance; i++)
+                        input += ")";
+                    input = Double.toString(parser.parse(input));
+                } catch (ArithmeticException e) {
+                    text.setText("Error: " + e.getMessage());
+                    input = "";
+                }
+                text.setText(input);
             }
         });
 
